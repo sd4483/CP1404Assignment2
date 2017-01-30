9389a5b86a829f7a52ff6082148b0f8e9d86aba9
sd4483 <sudheer.paturi@hotmail.com>
2017-01-31 04:15:16 +0800

    final

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from kivy.uix.button import Button
 
 class ReadingListApp(App):
     def __init__(self, **kwargs):
+        "declaring variables for class book and booklist"
         super(ReadingListApp, self).__init__(**kwargs)
         book_lists = BookList()
         book_items = Book()
@@ -25,20 +26,23 @@ class ReadingListApp(App):
         self.book_lists.listbooks()
 
     def build(self):
+        """loading kv file"""
         self.title = "Reading List 2.0 by Sudheer Paturi"
         self.root = Builder.load_file("app.kv")
         return self.root
 
     def on_start(self):
+        """initiates the program"""
         self.book_button(way='req')
         self.root.ids.required_book_button.state = 'down'
         self.root.ids.completed_book_button.state = 'normal'
 
     def on_stop(self):
+        "runs after program ends"
         self.book_lists.save_books()
 
     def add_books(self, text_title, text_author, text_pages):
-
+        """kivy code for add books function"""
         try:
             book_pages = int(text_pages)
             if text_title=="" or text_author=="" or text_pages=="":
@@ -72,17 +76,15 @@ class ReadingListApp(App):
                 self.root.ids.description.text = "{} marked as completed".format(each.title)
 
     def completed_books(self,instance):
-
         title = instance.text
         for each in self.book_lists.booklists:
             if each.title == title:
                 self.root.ids.description.text = "{} (completed)".format(each)
 
     def book_button(self, way):
+        """book button for widget box"""
         self.clear_books()
-
-        if way == "req":
-
+        if way == "req":#button for required books
             for each in range(len(self.book_lists.booklists)):
                 if self.book_lists[each].status == 'r':
 
@@ -90,7 +92,7 @@ class ReadingListApp(App):
                     temp_button.bind(on_release=self.required_books)
 
                     page_check = self.book_lists.extraPages(self.book_lists[each].pages)
-
+                    #checking length of pages
                     if page_check == True:
                         temp_button.background_color = 0, 1, 1, 1
                     else:
@@ -102,10 +104,8 @@ class ReadingListApp(App):
             self.root.ids.book_pages.text = total_required_pages
             self.root.ids.description.text = "Click book to mark them as completed"
 
-        elif way == "com":
-
+        elif way == "com":#button for completed books
             for each in range(len(self.book_lists.booklists)):
-
                 if self.book_lists[each].status == 'c':
                     temp_button = Button(text=self.book_lists[each].title)
                     temp_button.bind(on_release=self.completed_books)
@@ -116,19 +116,23 @@ class ReadingListApp(App):
             self.root.ids.book_pages.text = total_completed_pages
 
     def select_required_books(self):
+        """runs after list required is selected"""
         self.book_button(way='req')
         self.root.ids.required_book_button.state = 'down'
         self.root.ids.completed_book_button.state = 'normal'
 
     def select_completed_books(self):
+        """runs after list completed in selected"""
         self.book_button(way='com')
         self.root.ids.required_book_button.state = 'normal'
         self.root.ids.completed_book_button.state = 'down'
 
     def clear_books(self):
+        """clears button"""
         self.root.ids.box.clear_widgets()
 
     def clear_text(self):
+        """clears text in the add book fields"""
         self.root.ids.text_title.text = ""
         self.root.ids.text_author.text = ""
         self.root.ids.text_pages.text = ""
